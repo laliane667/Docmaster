29f61169a11e9aa0bc44cfd127900202d8f30e1a
Elian Lamouroux <elian.lamouroux@gmail.com>
2024-06-06 22:46:39 +0200

    Parameters

--- src/routes/Parameters.jsx
+++ src/routes/Parameters.jsx
@@ -1,16 +1,14 @@
 import { useEffect, useState } from "react"
-import { Card, Group, Stack, Text, Title, rem, Divider, Button, Pagination, Center, Badge } from "@mantine/core"
+import { Paper, Card, Group, Stack, Text, Title, rem, Divider, Button, Pagination, Center, Badge } from "@mantine/core"
 
 export default function Parameters() {
   const [activePage, setPage] = useState(0)
 
   return (
-    <Stack gap="lg" align="center">
-      <Card shadow="sm" radius="lg" w="100%" maw={500}>
-        <Title ta="center" mx="md" order={3} fw={600}>
-          Mes paramètres
-        </Title>
-      </Card>
-    </Stack>
+
+    <Paper>
+
+    </Paper>
+
   )
 }
